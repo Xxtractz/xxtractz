fde611502b2a4ebda878b62549610ce49ace4a0f
Musa Baloyi <musambaloyi@gmail.com>
2025-01-10 00:08:39 +0200

    #commit restructuring

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,20 @@
 import Head from 'next/head';
+import Link from 'next/link';
 import styles from '../styles/Home.module.css';
+import '../app/global.css';
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Xxtractz | Musa Martin Baloyi</title>
+        <title>Xxtractz | Musa Baloyi</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
+        <main>
+            <h1 className={styles.title}>
+                Read <Link href="/blog/post"> This</Link>
+            </h1>
+        </main>
     </div>
   );
 }
